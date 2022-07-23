c853688f9402ecd7a2c935192b9e42e389b372ba
Diego Moreno <diegomendesmoreno@gmail.com>
2022-07-23 18:05:17 -0300

    Classes Content and HttpClient done

--- src/App.java
+++ src/App.java
@@ -1,11 +1,7 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -26,35 +22,32 @@ public class App {
         String apiKey = prop.getProperty("prop.api.key");
         String endpoint = prop.getProperty("prop.api.endpoint");
 
-        // Establish a HTTP conection and search the top 250 movies in IMDB
-        URI address = URI.create(endpoint + apiKey);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(address).GET().build();
-        var response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        // Establish a HTTP conection and get JSON from API
+        var httpClient = new MyHttpClient();
+        String body = httpClient.getData(endpoint + apiKey);
 
         // Extract only necessary data (title, image, etc.)
         var parser = new JsonParser();
-        List<Map<String, String>> movieList = parser.parse(body);
+        List<Map<String, String>> contentList = parser.parse(body);
 
         // Manipulate and display data
-        for(int i = 0; i < 10; i++) {
-            Map<String,String> movie = movieList.get(i);
+        for(int i = 0; i < 5; i++) {
+            Map<String,String> content = contentList.get(i);
 
             // Get URL of the bigger image
-            String imageUrl = movie.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
+            String imageUrl = content.get("image").replaceAll("(@+)(.*).jpg$", "$1.jpg");
 
             // List Movies in the Terminal
-            System.out.println("Title : " + movie.get("title"));
-            System.out.println("Rank  : " + movie.get("rank"));
-            System.out.println("Rating: " + movie.get("imDbRating"));
+            System.out.println("Title : " + content.get("title"));
+            System.out.println("Rank  : " + content.get("rank"));
+            System.out.println("Rating: " + content.get("imDbRating"));
             System.out.println("Poster: " + imageUrl);
             System.out.println();
 
             // Generate Sticker Images
             InputStream inputStream = new URL(imageUrl).openStream();
             String movieComment = "IRADO!";
-            String imageFileName = movie.get("rank") + " - " + movie.get("title") + ".png";
+            String imageFileName = content.get("rank") + " - " + content.get("title") + ".png";
             
             StickerGenerator generator = new StickerGenerator();
             generator.create(inputStream, imageFileName, movieComment);
